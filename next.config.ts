6c17ebe55f037d3eb7c98f482b65efe4c8462e98
Matt Cassara <cassaram09@gmail.com>
2024-10-26 17:09:31 -0600

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,51 +3,50 @@ import withSerwistInit from "@serwist/next";
 const withSerwist = withSerwistInit({
   swSrc: "src/service-worker/sw.ts",
   swDest: "public/sw.js",
-  // disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig = {
   sassOptions: {
     additionalData: `@use "./src/styles/_base.scss";`,
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-        ],
-      },
-      {
-        source: "/sw.js",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/javascript; charset=utf-8",
-          },
-          {
-            key: "Cache-Control",
-            value: "no-cache, no-store, must-revalidate",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self'",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           key: "X-Content-Type-Options",
+  //           value: "nosniff",
+  //         },
+  //         {
+  //           key: "X-Frame-Options",
+  //           value: "DENY",
+  //         },
+  //         {
+  //           key: "Referrer-Policy",
+  //           value: "strict-origin-when-cross-origin",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       source: "/sw.js",
+  //       headers: [
+  //         {
+  //           key: "Content-Type",
+  //           value: "application/javascript; charset=utf-8",
+  //         },
+  //         {
+  //           key: "Cache-Control",
+  //           value: "no-cache, no-store, must-revalidate",
+  //         },
+  //         {
+  //           key: "Content-Security-Policy",
+  //           value: "default-src 'self'; script-src 'self'",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default withSerwist(nextConfig);
