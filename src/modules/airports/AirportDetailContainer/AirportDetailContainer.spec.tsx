02458e4cfe4f353a26438c731e212c5812ee2a76
Matt Cassara <cassaram09@gmail.com>
2024-10-31 08:19:03 -0600

    fix

--- src/modules/airports/AirportDetailContainer/AirportDetailContainer.spec.tsx
+++ src/modules/airports/AirportDetailContainer/AirportDetailContainer.spec.tsx
@@ -1,8 +1,5 @@
-import {render} from 'test';
-import AirportDetailContainer from './AirportDetailContainer';
-
-describe('AirportDetailContainer', () => {
-  it('renders', async () => {
-    throw new Error('You need to write a test for this component!');
+describe("AirportDetailContainer", () => {
+  it("renders", async () => {
+    throw new Error("You need to write a test for this component!");
   });
 });
